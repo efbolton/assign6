38eef40ecf7466a006f73996ab7fc29fb3ffc65b
efbolton <EFBOLTON@HOTMAIL.COM>
2021-09-29 12:36:33 -0400

    1st android commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>iosTeam branch!   **********</Text>
+      <Text>androidTeam branch!   **********</Text>
       <StatusBar style="auto" />
     </View>
   );
