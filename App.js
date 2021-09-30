c4e3054ef169d153d811a6e567b29d2c82e8d82c
efbolton <EFBOLTON@HOTMAIL.COM>
2021-09-30 09:07:24 -0400

    demo text change ios

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>iosTeam branch!   **********</Text>
+      <Text>iosTeam branch!   *****ios*****</Text>
       <StatusBar style="auto" />
     </View>
   );
